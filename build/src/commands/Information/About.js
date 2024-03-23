6eb1d45a6772800966c19b89571d9e9c8cdcd51f
Drxnzer <132266013+Drxnzer@users.noreply.github.com>
2024-03-23 15:15:58 +0530

    Update About.js

--- build/src/commands/Information/About.js
+++ build/src/commands/Information/About.js
@@ -9,29 +9,20 @@ export default class About extends OrasCommand {
         this.aliases = [];
         this.cat = "info";
         this.exec = async (message, args, prefix) => {
-            return message.reply({
-                embeds: [
-                    this.client.utils.embed()
-                        .setAuthor({
-                            name: `${message.author.username}`,
-                            iconURL: message.author.displayAvatarURL(),
-                        })
-                        .setTitle(`About`)
-                        .setDescription(`You can type \`${prefix}help\` for usable commands. If you want this bot in your server, type \`${prefix}invite\`. \n\n<:owner_1:1212331707261390888> • **OWNERS**\nstorm_xd#0000\n<:command:1211358351234179092> • **DEVELOPERS**\nDhruv_xd#0000`)
-                        .setFooter({
-                            text: `Thanks For Selecting ${this.client.user.username}`,
-                            iconURL: this.client.user.displayAvatarURL({ dynamic: true }),
-                        }),
-                ],
-                components: [
-                    this.client.utils.actionRow([
-                        this.client.utils.button("link", "Invite Me", null, null, `${this.client.config.botinvite}`),
-                        this.client.utils.button("link", "Support", null, null, `${this.client.config.server}`),
-                        this.client.utils.button("link", "Premium", null, null, `${this.client.config.server}`),
-                    ]),
-                ],
-            });
+            const embed = {
+                author: {
+                    name: message.author.username,
+                    iconURL: message.author.displayAvatarURL(),
+                },
+                title: "About",
+                description: "This is a music bot.",
+                footer: {
+                    text: `Thanks For Selecting ${this.client.user.username}`,
+                    iconURL: this.client.user.displayAvatarURL({ dynamic: true }),
+                },
+            };
+
+            return message.reply({ embeds: [embed] });
         };
     }
-                            }
-                                                                                   
+}
