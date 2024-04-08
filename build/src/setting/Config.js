3e13d29880fe1d2587cb1861f3471d39b78044a7
Drxnzer <132266013+Drxnzer@users.noreply.github.com>
2024-04-08 23:06:02 +0530

    Update Config.js

--- build/src/setting/Config.js
+++ build/src/setting/Config.js
@@ -16,9 +16,7 @@ export default class OrasConfig extends Object {
     setupBgLink;
     constructor() {
         super();
-        this.token =
-            "";
-        this.botid = ""
+        this.token = process.env.TOKEN || ""; // Access token from environment variable
         this.prefix = ">";
         this.nodes = [
             {
@@ -32,10 +30,9 @@ export default class OrasConfig extends Object {
               url: `lava.link:80`,
               auth: `kronix`,
               secure: false,
-          }, // Dont Change The Name Of Nodes
+          }, // Don't Change The Name Of Nodes
         ];
-        this.voteApi =
-            "";
+        this.voteApi = "";
         this.webhooks = {
             guildCreate: "https://discord.com/api/webhooks/1214847946428911676/-Wq2p66aXHxpXvdxQl7L9_Lv4kE0kStIN0PElfZPWEhUCFlq26UZHsC0ISNdOOQFoYaD",//put webhook
             guildDelete: "https://discord.com/api/webhooks/1214847946428911676/-Wq2p66aXHxpXvdxQl7L9_Lv4kE0kStIN0PElfZPWEhUCFlq26UZHsC0ISNdOOQFoYaD",//put webhook
@@ -45,7 +42,7 @@ export default class OrasConfig extends Object {
         this.botinvite = ``;//put you bot invite link
         this.spotiId = "6c31645ffb004ab8b44d06f7b96d1b66";
         this.spotiSecret = "3618fdc0b4824cfd91a8d425dac32987";
-        this.owners = [""];//put you discord id
+        this.owners = [""];//put your discord id
         this.color = "#880808";
         this.supportId = ""; //Server Id
         this.spotiNodes = [
@@ -62,11 +59,10 @@ export default class OrasConfig extends Object {
               port: 80,
               password: `kronix`,
               secure: false,
-          }, // Dont Change The Name Of Nodes
+          }, // Don't Change The Name Of Nodes
         ];
         this.voteUrl = "https://top.gg/bot/1027828697828433980/vote";
-        this.setupBgLink =
-            "https://media.discordapp.net/attachments/1190943041922535517/1190943144158691369/live_now.png?ex=65a3a33c&is=65912e3c&hm=3f442c1bd2f8b61e3be1f1c625d891df2b90334098765ca174750746912df20d&=&format=webp&quality=lossless&width=1000&height=562";
+        this.setupBgLink = "https://media.discordapp.net/attachments/1190943041922535517/1190943144158691369/live_now.png?ex=65a3a33c&is=65912e3c&hm=3f442c1bd2f8b61e3be1f1c625d891df2b90334098765ca174750746912df20d&=&format=webp&quality=lossless&width=1000&height=562";
     }
 }
 //# sourceMappingURL=Config.js.map
